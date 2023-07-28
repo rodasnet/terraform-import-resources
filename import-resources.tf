f69d89c7cc15f6c02b66b3578032b39a37e43a64
Daniel Rodas <daniel.rodas1@hotmail.com>
2023-07-27 19:03:23 -0700

    Update import-resources.tf

--- import-resources.tf
+++ import-resources.tf
@@ -2,6 +2,8 @@
 resources to import into terraform state
 https://developer.hashicorp.com/terraform/language/import
 -generate-config-out
+Warning: Config generation is experimental
+Generating configuration during import is currently experimental, and the generated configuration format may change in future versions.
 
 VPC
 vpc-0eb820fc1bf525783
@@ -16,6 +18,14 @@ ARN: arn:aws:ec2:us-east-1:719774287182:instance/i-09873e4b0fcb4fc22
 S3 Bucket
 sample-s3-bucket-01
 ARN: arn:aws:s3:::sample-s3-bucket-01
+
+Notes:
+Error importing EC2
+https://app.terraform.io/app/rodasnet/workspaces/Workspace-Import-Resources/runs/run-jz5fDM9e4Nwh5Qp6
+Error: Conflicting configuration arguments
+with aws_instance.HelloWorld-2
+on generated-run-jz5fDM9e4Nwh5Qp6.tf line 15:
+"ipv6_addresses": conflicts with ipv6_address_count
 
 */
 
