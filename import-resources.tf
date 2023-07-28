4f2e4e0b6bab66aadd36a636f59916562ea96021
Daniel Rodas <daniel.rodas1@hotmail.com>
2023-07-27 19:45:58 -0700

    Update import-resources.tf

--- import-resources.tf
+++ import-resources.tf
@@ -29,6 +29,8 @@ on generated-run-jz5fDM9e4Nwh5Qp6.tf line 15:
 
 */
 // After Terraform code has been generated for resource remove import statement.
+// After running and importing code via TFC, plan is clean with no changes. 
+// https://app.terraform.io/app/rodasnet/workspaces/Workspace-Import-Resources/runs/run-gUrkdbMbWAF22D1f
 # import {
 #   to = aws_instance.HelloWorld-2
 #   id = "i-09873e4b0fcb4fc22"
